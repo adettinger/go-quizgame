3d4f828f86d2594063f0c787209b5b7898941ca6
adettinger <apddeapp@gmail.com>
2026-02-03 15:01:08 +0100

    Nvabar with basic style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,25 @@ import './App.css'
 import Welcome from './pages/Welcome';
 import NotFound from './pages/NotFound';
 import { ProblemsPage } from './pages/Problems';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
     <Router>
       <div className='App'>
-        {/* TODO: NavBar */}
-        <span>Quizgame NavBar</span>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/*" element={<NotFound />} />
-          <Route path="/problems" element={<ProblemsPage />} />
-        </Routes>
+        <NavBar />
+        <div className="container mt-4">
+          <div className='App-header'>
+            <h1>Quizgame</h1>
+          </div>
+
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/*" element={<NotFound />} />
+            <Route path="/problems" element={<ProblemsPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, NavLink } from 'react-router-dom';
+import './NavBar.scss';
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                                to="/"
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                                to="/problems"
+                            >
+                                Problems
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
